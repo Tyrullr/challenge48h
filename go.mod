6204733ccea4f14f195aace08eb34184d3fac7ca
chai serfaty <serfaty>
2025-03-19 12:16:41 +0100

    architecture + gestion des routes

---
+++ go.mod
@@ -0,0 +1,3 @@
+module site-BDD
+
+go 1.23.3
---
+++ internal/server.go
@@ -0,0 +1,24 @@
+package internal
+
+import (
+	"net/http"
+)
+
+// NewServer crée un serveur HTTP et configure les routes
+func NewServer() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	// Servir les fichiers statiques (CSS, JS, images)
+	fs := http.FileServer(http.Dir("static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Définition des routes
+	mux.HandleFunc("/", HomeHandler)
+	mux.HandleFunc("/actualite", ActualiteHandler)
+	mux.HandleFunc("/admin", AdminHandler)
+	mux.HandleFunc("/connection", ConnectionHandler)
+	mux.HandleFunc("/contact", ContactHandler)
+	mux.HandleFunc("/presentation", PresentationHandler)
+
+	return mux
+}
